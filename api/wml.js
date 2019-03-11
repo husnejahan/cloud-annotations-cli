18434dc19b65d81c683d67ce8dedcb264c3b42ab
Nick Bourdakos <bourdakos1@gmail.com>
2019-03-10 20:02:31 -0400

    give train basic functionality

--- api/wml.js
+++ api/wml.js
@@ -1,119 +1,125 @@
 const request = require('request-promise-native')
 
-module.exports = () => {
-  return {
-    authent: ''
+class WML {
+  constructor({ instance_id, username, password, url }) {
+    this._token = undefined
+    this._instanceId = instance_id
+    this._username = username
+    this._password = password
+    this._url = url
+  }
+
+  async authenticate() {
+    return request({
+      method: 'GET',
+      json: true,
+      url: `${this._url}/v3/identity/token`,
+      auth: {
+        user: this._username,
+        pass: this._password
+      }
+    }).then(body => {
+      return body.token
+    })
+  }
+
+  async createTrainingDefinition(name) {
+    if (!this._token) {
+      this._token = await this.authenticate()
+    }
+    const trainingDefinition = {
+      name: name,
+      framework: {
+        name: 'tensorflow',
+        version: '1.12',
+        runtimes: [
+          {
+            name: 'python',
+            version: '3.5'
+          }
+        ]
+      }
+    }
+    return request({
+      method: 'POST',
+      json: true,
+      url: `${this._url}/v3/ml_assets/training_definitions`,
+      auth: {
+        bearer: this._token
+      },
+      body: trainingDefinition
+    })
+  }
+
+  async addTrainingScript(trainingDefinition) {
+    if (!this._token) {
+      this._token = await this.authenticate()
+    }
+    return request(
+      'https://github.com/cloud-annotations/cloud-annotations-sdk/releases/download/v0.0.1-beta/object_detection.zip'
+    ).pipe(
+      request({
+        method: 'PUT',
+        json: true,
+        url: trainingDefinition.entity.training_definition_version.content_url,
+        auth: {
+          bearer: this._token
+        },
+        headers: {
+          'content-type': 'application/octet-stream'
+        }
+      })
+    )
+  }
+
+  async startTrainingRun(trainingDefinition, config) {
+    if (!this._token) {
+      this._token = await this.authenticate()
+    }
+    const connection = {
+      endpoint_url: `https://s3-api.${
+        config.credentials.cos.region
+      }.objectstorage.service.networklayer.com`,
+      access_key_id: config.credentials.cos.access_key_id,
+      secret_access_key: config.credentials.cos.secret_access_key
+    }
+    const trainingRun = {
+      model_definition: {
+        framework: {
+          name: trainingDefinition.entity.framework.name,
+          version: trainingDefinition.entity.framework.version
+        },
+        name: config.name,
+        author: {},
+        definition_href: trainingDefinition.metadata.url,
+        execution: {
+          command: `python3 -m wml.train_command --num-train-steps=${
+            config.trainingParams.steps
+          }`,
+          compute_configuration: { name: config.trainingParams.gpu }
+        }
+      },
+      training_data_reference: {
+        connection: connection,
+        source: { bucket: config.buckets.training },
+        type: 's3'
+      },
+      training_results_reference: {
+        connection: connection,
+        target: { bucket: config.buckets.output || config.buckets.training },
+        type: 's3'
+      }
+    }
+    return request({
+      method: 'POST',
+      json: true,
+      url: `${this._url}/v3/models`,
+      auth: {
+        bearer: this._token
+      },
+      body: trainingRun
+    })
   }
 }
-// const STEPS = 50
-// const GPU = 'v100x2'
-// const TRAINING_BUCKET = 'pepsi-coke-mountain-dew'
-// const OUTPUT_BUCKET = 'soda-output-dir'
 
-// const TRAINING_DEFINITION = {
-//   name: 'tf-object-detection',
-//   framework: {
-//     name: 'tensorflow',
-//     version: '1.12',
-//     runtimes: [
-//       {
-//         name: 'python',
-//         version: '3.5'
-//       }
-//     ]
-//   },
-//   command: `python3 -m wml.train_command --num-train-steps=${STEPS}`
-// }
-
-// const TRAINING_RUN = {
-//   model_definition: {
-//     framework: {
-//       name: TRAINING_DEFINITION.framework.name,
-//       version: TRAINING_DEFINITION.framework.version
-//     },
-//     name: 'tf-object-detection_training-run',
-//     author: {},
-//     definition_href:
-//       'https://us-south.ml.cloud.ibm.com/v3/ml_assets/training_definitions/2a2241a9-ccc2-415d-a92e-dfeb0594278d',
-//     execution: {
-//       command: TRAINING_DEFINITION.command,
-//       compute_configuration: { name: GPU }
-//     }
-//   },
-//   training_data_reference: {
-//     connection: {
-//       endpoint_url: COS_URL,
-//       access_key_id: COS_ACCESS_KEY_ID,
-//       secret_access_key: COS_SECRET_ACCESS_KEY
-//     },
-//     source: { bucket: TRAINING_BUCKET },
-//     type: 's3'
-//   },
-//   training_results_reference: {
-//     connection: {
-//       endpoint_url: COS_URL,
-//       access_key_id: COS_ACCESS_KEY_ID,
-//       secret_access_key: COS_SECRET_ACCESS_KEY
-//     },
-//     target: { bucket: OUTPUT_BUCKET },
-//     type: 's3'
-//   }
-// }
-
-// module.exports = () => {
-//   let TOKEN = ''
-
-//   request({
-//     method: 'GET',
-//     json: true,
-//     url: `${ML_URL}/v3/identity/token`,
-//     auth: {
-//       user: ML_USERNAME,
-//       pass: ML_PASSWORD
-//     }
-//   })
-//     .then(body => {
-//       TOKEN = body.token
-//       return request({
-//         method: 'POST',
-//         json: true,
-//         url: `${ML_URL}/v3/ml_assets/training_definitions`,
-//         auth: {
-//           bearer: TOKEN
-//         },
-//         body: TRAINING_DEFINITION
-//       })
-//     })
-//     .then(body => {
-//       TRAINING_RUN.model_definition.definition_href = body.metadata.url
-//       return request(
-//         'https://github.com/cloud-annotations/cloud-annotations-sdk/releases/download/v0.0.1-beta/object_detection.zip'
-//       ).pipe(
-//         request({
-//           method: 'PUT',
-//           json: true,
-//           url: body.entity.training_definition_version.content_url,
-//           auth: {
-//             bearer: TOKEN
-//           },
-//           headers: {
-//             'content-type': 'application/octet-stream'
-//           }
-//         })
-//       )
-//     })
-//     .then(() => {
-//       return request({
-//         method: 'POST',
-//         json: true,
-//         url: `${ML_URL}/v3/models`,
-//         auth: {
-//           bearer: TOKEN
-//         },
-//         body: TRAINING_RUN
-//       })
-//     })
-//     .then(() => {
-//       console.log('done')
-//     })
-// }
+module.exports = WML
--- commands/train.js
+++ commands/train.js
@@ -1,7 +1,11 @@
 const yaml = require('js-yaml')
 const fs = require('fs')
+const WML = require('./../api/wml')
 
-module.exports = options => {
+module.exports = async options => {
   const config = yaml.safeLoad(fs.readFileSync('config.yaml'))
-  wml.authentic()
+  const wml = new WML(config.credentials.wml)
+  const trainingDefinition = await wml.createTrainingDefinition(config.name)
+  await wml.addTrainingScript(trainingDefinition)
+  await wml.startTrainingRun(trainingDefinition, config)
 }
